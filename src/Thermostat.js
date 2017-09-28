ee0bd5cb0f12be0e329f612dc413b0c531cd76d6
Nero Siva <neroshan12@gmail.com>
2017-09-28 23:36:33 +0100

    Add energy usage function

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -7,6 +7,7 @@ function Thermostat() {       // Created a Thermostat object constructor and giv
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
   this.powerSavingMode = true;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 };
 
 Thermostat.prototype.getCurrentTemperature = function() {   // prototype keyword to associate or bind the getCurrentTemperature function to our Thermostat
@@ -50,4 +51,14 @@ Thermostat.prototype.switchPowerSavingModeOn = function() {
 }
 Thermostat.prototype.resetTemperature = function() {
   this.temperature = this.DEFAULT_TEMPERATURE;
+}
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
 }
